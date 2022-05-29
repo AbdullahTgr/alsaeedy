0af1e465609766c3b037d944f6fa462e4e7bbc9c
AbdullahTgr <4multijob@gmail.com>
2022-05-29 17:49:57 +0200

    gf

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MV43QJV');</script>
+	<!-- End Google Tag Manager -->
 	@include('frontend.layouts.head')	 
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
@@ -14,9 +21,14 @@
 </script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9802244405698113"
      crossorigin="anonymous"></script>
+
+	 
 </head>
 <body class="js">
-	
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MV43QJV"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	{{-- <!-- Preloader -->
 	<div class="preloader">
 		<div class="preloader-inner" style="left: 45%;">
